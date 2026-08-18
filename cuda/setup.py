2ad8ce4d09fd64dd77787e727ef6da0b4ca4f3cd
Aditya Bhardwaj <149936576+adityabhardwaj02@users.noreply.github.com>
2026-08-18 06:47:58 +0530

    Create setup.py

---
+++ cuda/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+setup(
+    name="flash_attention_cuda",
+    ext_modules=[
+        CUDAExtension(
+            name="flash_attention_cuda",
+            sources=["flash_attention_kernel.cu"],
+            extra_compile_args={"cxx": ["-O3"], "nvcc": ["-O3", "--use_fast_math"]},
+        )
+    ],
+    cmdclass={"build_ext": BuildExtension},
+)
